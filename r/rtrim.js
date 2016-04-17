7dbf1dc05a4c0017a6678331684b960232826c3a
surunzi <surunzi@foxmail.com>
2016-04-17 22:02:38 +0800

    Dev: rtrim

--- r/rtrim.js
+++ r/rtrim.js
@@ -1,4 +1,17 @@
-// TODO
+/* Remove chars or white-spaces from end of string.
+ *
+ * |Name  |Type         |Desc                  |
+ * |-------------------------------------------|
+ * |str   |string       |The string to trim    |
+ * |chars |string\|array|The characters to trim|
+ * |return|string       |The trimmed string    |
+ *
+ * ```javascript
+ * rtrim(' abc  '); // -> ' abc'
+ * rtrim('_abc_', '_'); // -> '_abc'
+ * rtrim('_abc_', ['c', '_']); // -> '_ab'
+ * ```
+ */
 
 var regSpace = /\s+$/;
 
@@ -6,9 +19,9 @@ exports = function (str, chars)
 {
     if (chars == null) return str.replace(regSpace, '');
 
-    var end     = str.length - 1,
+    var end = str.length - 1,
         charLen = chars.length,
-        found   = true,
+        found = true,
         i, c;
 
     while (found && end >= 0)
---
+++ r/rtrim.test.js
@@ -0,0 +1,6 @@
+it('trim the end of string', function ()
+{
+    expect(rtrim(' abc  ')).to.equal(' abc');
+    expect(rtrim('_abc_', '_')).to.equal('_abc');
+    expect(rtrim('_abc_', ['c', '_'])).to.equal('_ab');
+});
