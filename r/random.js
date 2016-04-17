caaa9bd716e485be95df358e2c41c24126f20f56
surunzi <surunzi@foxmail.com>
2016-04-17 17:36:45 +0800

    Dev: random

--- r/random.js
+++ r/random.js
@@ -1,13 +1,19 @@
-/* Produces a random number between min and max (inclusive).
+/* Produces a random number between min and max(inclusive).
  *
- * |Name|Type|Desc|
- * |--------------|
- * |min|number|The minimum possible value|
- * |max|number|The maximum possible value|
- * |return|number|The random number|
+ * |Name  |Type  |Desc                      |
+ * |----------------------------------------|
+ * |min   |number|The minimum possible value|
+ * |max   |number|The maximum possible value|
+ * |return|number|The random number         |
+ *
+ * ```javascript
+ * random(1, 5); // -> an integer between 0 and 5
+ * random(5); // -> an integer between 0 and 5
+ * random(1.2, 5.2, true); /// -> a floating-point number between 1.2 and 5.2
+ * ```
  */
 
-exports = function (min, max)
+exports = function (min, max, floating)
 {
     if (max == null)
     {
@@ -15,5 +21,9 @@ exports = function (min, max)
         min = 0;
     }
 
-    return min + Math.floor(Math.random() * (max - min + 1));
+    var ret = Math.random() * (max - min + 1);
+
+    if (!floating) ret = Math.floor(ret);
+
+    return min + ret;
 };
