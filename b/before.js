831a4f7210eb4532d2af903079b9cfbda4657da6
surunzi <surunzi@foxmail.com>
2016-04-19 16:50:05 +0800

    Dev: before

---
+++ b/before.js
@@ -0,0 +1,27 @@
+/* Create a function that invokes less than n times.
+ *
+ * |Name  |Type    |Desc                                            |
+ * |----------------------------------------------------------------|
+ * |n     |number  |Number of calls at which fn is no longer invoked|
+ * |fn    |function|Function to restrict                            |
+ * |return|function|New restricted function                         |
+ *
+ * Subsequent calls to the created function return the result of the last fn invocation.
+ *
+ * ```javascript
+ * $(element).on('click', before(5, function() {}));
+ * // -> allow function to be call 4 times at last.
+ * ```
+ */
+
+exports = function (n, fn)
+{
+    var memo;
+
+    return function ()
+    {
+        if (--n > 0) memo = fn.apply(this, arguments);
+        if (n <= 1) fn = null;
+        return memo;
+    };
+};
---
+++ b/before.test.js
@@ -0,0 +1,14 @@
+it('restrict function invoke times', function ()
+{
+    var count = 0;
+    var fn = before(5, function () { count++ });
+    fn();
+    expect(count).to.equal(1);
+    fn();
+    expect(count).to.equal(2);
+    fn();
+    fn();
+    fn();
+    fn();
+    expect(count).to.equal(4);
+});
