6a08c247a855f4e6cee6c9da3d1ccee2f699d170
surunzi <surunzi@foxmail.com>
2016-04-19 11:08:57 +0800

    Dev: min

---
+++ m/min.js
@@ -0,0 +1,26 @@
+/* Get minimum value of given numbers.
+ *
+ * |Name  |Type     |Desc                |
+ * |-------------------------------------|
+ * |num   |...number|Numbers to calculate|
+ * |return|number   |Minimum value       |
+ *
+ * ```javascript
+ * min(2.3, 1, 4.5, 2); // 1
+ * ```
+ */
+
+_('toArr');
+
+exports = function ()
+{
+    var arr = toArr(arguments),
+        ret = arr[0];
+
+    for (var i = 1, len = arr.length; i < len; i++)
+    {
+        if (arr[i] < ret) ret = arr[i];
+    }
+
+    return ret;
+};
---
+++ m/min.test.js
@@ -0,0 +1,4 @@
+it('get minimum value', function ()
+{
+    expect(min(1, 2, 8, 4)).to.equal(1);
+});
