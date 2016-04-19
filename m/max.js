d541450bc256bdf32419a7db4e319ec4f722624a
surunzi <surunzi@foxmail.com>
2016-04-19 11:04:03 +0800

    Dev: max

---
+++ m/max.js
@@ -0,0 +1,26 @@
+/* Get maximum value of given numbers.
+ *
+ * |Name  |Type     |Desc                |
+ * |-------------------------------------|
+ * |num   |...number|Numbers to calculate|
+ * |return|number   |Maximum value       |
+ *
+ * ```javascript
+ * max(2.3, 1, 4.5, 2); // 4.5
+ * ```
+ */
+
+_('toArr');
+
+exports = function ()
+{
+    var arr = toArr(arguments),
+        ret = arr[0];
+
+    for (var i = 1, len = arr.length; i < len; i++)
+    {
+        if (arr[i] > ret) ret = arr[i];
+    }
+
+    return ret;
+};
---
+++ m/max.test.js
@@ -0,0 +1,4 @@
+it('get maximum value', function ()
+{
+    expect(max(1, 2, 8, 4)).to.equal(8);
+});
