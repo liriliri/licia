ab4f2b5a1b6520ed357f5305d0ffb30a99be54a2
surunzi <surunzi@foxmail.com>
2016-04-20 19:30:59 +0800

    Dev: toArr

--- s/safeCb.js
+++ s/safeCb.js
@@ -2,11 +2,11 @@
  * safeCb: Create callback based on input value.
  */
 
-_('isFn isObj optimizeCb matcher');
+_('isFn isObj optimizeCb matcher identity');
 
 exports = function (val, ctx, argCount)
 {
-    if (val == null) return function (val) { return val };
+    if (val == null) return identity;
 
     if (isFn(val)) return optimizeCb(val, ctx, argCount);
 
--- t/toArr.js
+++ t/toArr.js
@@ -1,10 +1,27 @@
-_('isArrLike isStr map isArr');
+/* Convert value to an array.
+ *
+ * |Name  |Type |Desc            |
+ * |-----------------------------|
+ * |val   |*    |Value to convert|
+ * |return|array|Converted array |
+ *
+ * ```javascript
+ * toArr({a: 1, b: 2}); // -> [1, 2]
+ * toArr('abc'); // -> ['a', 'b', 'c']
+ * toArr(1); // -> []
+ * toArr(null); // -> []
+ * ```
+ */
 
-exports = function (obj)
+_('isArrLike isStr map isArr identity');
+
+exports = function (val)
 {
-    if (isArr(obj)) return obj;
+    if (!val) return [];
 
-    return isArrLike(obj) && !isStr(obj)
-           ? map(obj, function (val) { return val })
-           : [obj];
+    if (isArr(val)) return val;
+
+    if (isArrLike(val) || isObj(val)) return map(val);
+
+    return [val];
 };
