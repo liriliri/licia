69301758cb1bebaae565b437d2b62e2526508033
surunzi <surunzi@foxmail.com>
2016-04-22 21:48:08 +0800

    Dev: identity

--- i/identity.js
+++ i/identity.js
@@ -1,3 +1,15 @@
+/* Return the first argument given.
+ *
+ * |Name  |Type|Desc       |
+ * |-----------------------|
+ * |val   |*   |Any value  |
+ * |return|*   |Given value|
+ *
+ * ```javascript
+ * identity('a'); // -> 'a'
+ * ```
+ */
+
 exports = function (val)
 {
     return val;
---
+++ i/identity.test.js
@@ -0,0 +1,5 @@
+it('return the first argument', function ()
+{
+    expect(identity(1)).to.equal(1);
+    expect(identity('a')).to.equal('a');
+});
