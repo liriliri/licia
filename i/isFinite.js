31dfaca6ab95f75537797fab7dafd35ab40f12fe
surunzi <surunzi@foxmail.com>
2016-04-24 19:43:54 +0800

    Dev: isFinite

---
+++ i/isFinite.js
@@ -0,0 +1,22 @@
+/* Check if value is a finite primitive number.
+ *
+ * |Name  |Type   |Desc                            |
+ * |-----------------------------------------------|
+ * |val   |*      |Value to check                  |
+ * |return|boolean|True if value is a finite number|
+ *
+ * ```javascript
+ * isFinite(3); // -> true
+ * isFinite(Infinity); // -> false
+ * ```
+ */
+
+_('root');
+
+var nativeIsFinite = root.isFinite,
+    nativeIsNaN = root.isNaN;
+
+function exports(val)
+{
+    return nativeIsFinite(val) && !nativeIsNaN(parseFloat(val));
+}
