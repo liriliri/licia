4867943e766ebd8c1d139ab940f19102e02a50c1
surunzi <surunzi@foxmail.com>
2017-02-16 16:02:10 +0800

    Add: isEl test

--- i/isEl.test.js
+++ i/isEl.test.js
@@ -1,1 +1,11 @@
-// Browser test is not supported yet.
+it('true', function ()
+{
+    expect(isEl(document.body)).to.be.true;
+    expect(isEl(document.createElement('div'))).to.be.true;
+});
+
+it('false', function ()
+{
+    expect(isEl(document.createTextNode('test'))).to.be.false;
+    expect(isEl(document.createComment('test'))).to.be.false;
+});
