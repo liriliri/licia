2de3cfbc221440c758f5b43e85571dbe220a16f2
surunzi <surunzi@foxmail.com>
2017-03-12 13:52:00 +0800

    Dev: Trim test

--- l/ltrim.test.js
+++ l/ltrim.test.js
@@ -3,4 +3,5 @@ it('basic', function ()
     expect(ltrim(' abc  ')).to.equal('abc  ');
     expect(ltrim('_abc_', '_')).to.equal('abc_');
     expect(ltrim('_abc_', ['a', '_'])).to.equal('bc_');
+    expect(ltrim('_abc_', 'a_')).to.equal('bc_');
 });
--- t/trim.test.js
+++ t/trim.test.js
@@ -3,4 +3,5 @@ it('basic', function ()
     expect(trim(' abc  ')).to.equal('abc');
     expect(trim('_abc_', '_')).to.equal('abc');
     expect(trim('_abc_', ['a', 'c', '_'])).to.equal('b');
+    expect(trim('_abc_', 'ac_')).to.equal('b');
 });
