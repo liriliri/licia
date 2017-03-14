3e446c246fb489063c8569805414905c945fe2ab
surunzi <surunzi@foxmail.com>
2017-03-14 15:17:27 +0800

    Dev: Promise test space indent

--- p/Promise.test.js
+++ p/Promise.test.js
@@ -1,79 +1,79 @@
 var adapter = {
     resolved: Promise.resolve,
-	rejected: Promise.rejected,
-	deferred: function() 
+    rejected: Promise.rejected,
+    deferred: function() 
     {
-		var obj = {};
-		var prom = new Promise(function(resolve, reject) 
+        var obj = {};
+        var prom = new Promise(function(resolve, reject) 
         {
-			obj.resolve = resolve;
-			obj.reject = reject;
-		});
-		obj.promise = prom;
+            obj.resolve = resolve;
+            obj.reject = reject;
+        });
+        obj.promise = prom;
 
-		return obj;
-	}
+        return obj;
+    }
 };
 
 require('promises-aplus-tests').mocha(adapter);
 
 it('all', function (done) 
 {
-	expect(Promise.all([])).to.be.an.instanceof(Promise);
+    expect(Promise.all([])).to.be.an.instanceof(Promise);
 
-	var step = 0;
+    var step = 0;
 
-	Promise.all([new Promise(function (resolve) 
-	{
-		resolve('a');
-	}), new Promise(function (resolve) 
-	{
-		resolve('b');
-	})]).then(function (results) 
-	{
-		expect(results).to.eql(['a', 'b']);
-		step++;
-		if (step === 2) done();
-	});
+    Promise.all([new Promise(function (resolve) 
+    {
+        resolve('a');
+    }), new Promise(function (resolve) 
+    {
+        resolve('b');
+    })]).then(function (results) 
+    {
+        expect(results).to.eql(['a', 'b']);
+        step++;
+        if (step === 2) done();
+    });
 
-	Promise.all([new Promise(function (resolve) 
-	{
-		resolve('a');
-	}), new Promise(function (resolve, reject) 
-	{
-		throw new Error('error');
-	})]).catch(function (val) 
-	{
-		expect(val).to.be.an('error');
-		step++;
-		if (step === 2) done();
-	});
+    Promise.all([new Promise(function (resolve) 
+    {
+        resolve('a');
+    }), new Promise(function (resolve, reject) 
+    {
+        throw new Error('error');
+    })]).catch(function (val) 
+    {
+        expect(val).to.be.an('error');
+        step++;
+        if (step === 2) done();
+    });
 });
 
 it('reject', function (done) 
 {
-	var promise = Promise.reject('a');
+    var promise = Promise.reject('a');
 
-	promise.catch(function (val) 
-	{
-		expect(val).to.equal('a');
-		done();
-	});
+    promise.catch(function (val) 
+    {
+        expect(val).to.equal('a');
+        done();
+    });
 });
 
 it('race', function (done) 
 {
-	expect(Promise.race([])).to.be.an.instanceof(Promise);
+    expect(Promise.race([])).to.be.an.instanceof(Promise);
 
-	Promise.race([new Promise(function (resolve) 
-	{
-		resolve('a');
-	}), new Promise(function (resolve) 
-	{
-		setTimeout(function () { resolve('b') }, 50);
-	})]).then(function (val) 
-	{
-		expect(val).to.equal('a');
-		done();
-	});
+    Promise.race([new Promise(function (resolve) 
+    {
+        resolve('a');
+    }), new Promise(function (resolve) 
+    {
+        setTimeout(function () { resolve('b') }, 50);
+    })]).then(function (val) 
+    {
+        expect(val).to.equal('a');
+        done();
+    });
 });
