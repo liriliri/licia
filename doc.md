073d3ae487c0da499c80c5b07edf47238e83aecb
surunzi <surunzi@foxmail.com>
2017-07-27 11:13:24 +0800

    Add: fileSize

--- doc.md
+++ doc.md
@@ -1747,6 +1747,23 @@ fetch('test.json', {
 });
 ```
 
+## fileSize 
+
+Turn bytes into human readable file size.
+
+|Name  |Type  |Desc              |
+|------|------|------------------|
+|bytes |number|File bytes        |
+|return|string|Readable file size|
+
+```javascript
+fileSize(5); // -> '5'
+fileSize(1500); // -> '1.46K'
+fileSize(1500000); // -> '1.43M'
+fileSize(1500000000); // -> '1.4G'
+fileSize(1500000000000); // -> '1.36T'
+```
+
 ## filter 
 
 Iterates over elements of collection, returning an array of all the values that pass a truth test.
---
+++ f/fileSize.js
@@ -0,0 +1,27 @@
+/* Turn bytes into human readable file size.
+ * 
+ * |Name  |Type  |Desc              |
+ * |------|------|------------------|
+ * |bytes |number|File bytes        |
+ * |return|string|Readable file size|
+ * 
+ * ```javascript
+ * fileSize(5); // -> '5'
+ * fileSize(1500); // -> '1.46K'
+ * fileSize(1500000); // -> '1.43M'
+ * fileSize(1500000000); // -> '1.4G'
+ * fileSize(1500000000000); // -> '1.36T'
+ * ```
+ */
+
+function exports(bytes) 
+{
+    if (bytes <= 0) return '0';
+
+    var suffixIdx = Math.floor(Math.log(bytes) / Math.log(1024)),
+        val = bytes / Math.pow(2, suffixIdx * 10);
+
+    return +val.toFixed(2) + suffixList[suffixIdx];
+}
+
+var suffixList = ['', 'K', 'M', 'G', 'T'];
