371928978c56854f97296739df5e8d17e522e94a
julyL <849424162@qq.com>
2017-09-19 15:20:33 +0800

    Fix: safeGet not work in object with array

--- s/safeGet.test.js
+++ s/safeGet.test.js
@@ -1,7 +1,7 @@
 it('basic', function ()
 {
-    var obj = {a: {aa: {aaa: 1}}};
-    expect(safeGet(obj, 'a.aa.aaa')).to.equal(1);
-    expect(safeGet(obj, ['a', 'aa'])).to.eql({aaa: 1});
+    var obj = {a: {aa: [2,{b:33}]}};
+    expect(safeGet(obj, 'a.aa[0]')).to.equal(2);
+    expect(safeGet(obj, ['a', 'aa','1','b'])).to.eql(33);
     expect(safeGet(obj, 'a.b')).to.be.an('undefined');
 });
