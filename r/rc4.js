1b967f3c390ce30c241b5af33c7e641dde97a746
Victor Nascimento <victormota15@gmail.com>
2018-08-08 22:57:31 -0300

    Create rc4.js

---
+++ r/rc4.js
@@ -0,0 +1,31 @@
+/*
+ * RC4 symmetric cipher encryption/decryption
+ *
+ * @license Public Domain
+ * @param string key - secret key for encryption/decryption
+ * @param string str - string to be encrypted/decrypted
+ * @return string
+ */
+function rc4(key, str) {
+	var s = [], j = 0, x, res = '';
+	for (var i = 0; i < 256; i++) {
+		s[i] = i;
+	}
+	for (i = 0; i < 256; i++) {
+		j = (j + s[i] + key.charCodeAt(i % key.length)) % 256;
+		x = s[i];
+		s[i] = s[j];
+		s[j] = x;
+	}
+	i = 0;
+	j = 0;
+	for (var y = 0; y < str.length; y++) {
+		i = (i + 1) % 256;
+		j = (j + s[i]) % 256;
+		x = s[i];
+		s[i] = s[j];
+		s[j] = x;
+		res += String.fromCharCode(str.charCodeAt(y) ^ s[(s[i] + s[j]) % 256]);
+	}
+	return res;
+}
