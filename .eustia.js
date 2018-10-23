fceacec8a4f0b0fbce1ec1961c1016be67dd242b
redhoodsu <redhoodsu@tencent.com>
2018-10-23 11:22:04 +0800

    Move module codes to src

--- .eustia.js
+++ .eustia.js
@@ -1,6 +1,6 @@
 module.exports = {
     files: ['lib/**/*.js', 'bin/*.js', '*.js', 'script/*.js'],
-    library: '$_abcdefghijklmnopqrstuvwxyz'.split(''),
+    library: '$_abcdefghijklmnopqrstuvwxyz'.split('').map(val => 'src/' + val),
     output: 'lib/util.js',
     exclude: 'js',
     removeComments: true,
