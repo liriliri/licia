99e17fb7bb91783a48cfd8ea884e1dc0fc37ac5a
surunzi <surunzi@foxmail.com>
2019-12-16 00:14:33 +0800

    chore: small changes

--- src/i/isArrLike.test.js
+++ src/i/isArrLike.test.js
@@ -1,10 +1,11 @@
-it('true', function() {
-    expect(isArrLike([1, 2, 3])).to.be.true;
-    expect(isArrLike('abc')).to.be.true;
-    expect(isArrLike({ length: 2, 0: '0', 1: '1' })).to.be.true;
+it('true', () => {
+    test([
+        [[1, 2, 3], true],
+        ['abc', true],
+        [{ length: 2, 0: '0', 1: '1' }, true]
+    ]);
 });
 
-it('false', function() {
-    expect(isArrLike(function() {})).to.be.false;
-    expect(isArrLike({})).to.be.false;
+it('false', () => {
+    test([[function() {}, false], [{}, false]]);
 });
--- src/i/isClose.test.js
+++ src/i/isClose.test.js
@@ -1,12 +1,7 @@
-it('basic', function() {
-    expect(isClose(1, 1.0000000001)).to.be.true;
-    expect(isClose(1, 2)).to.be.false;
+it('basic', () => {
+    test([[1, 1.0000000001, true], [1, 2, false]]);
 });
 
-it('relative tolerance', function() {
-    expect(isClose(1, 1.2, 0.3)).to.be.true;
-});
+it('relative tolerance', () => test([[1, 1.2, 0.3, true]]));
 
-it('absolute tolerance', function() {
-    expect(isClose(1, 1.2, 0.1, 0.3)).to.be.true;
-});
+it('absolute tolerance', () => test([[1, 1.2, 0.1, 0.3, true]]));
