15143a9a6d6791fe2f34403d63420515fc5e00f1
surunzi <redhoodsu@tencent.com>
2020-04-03 10:45:37 +0800

    test: kill

--- src/k/kill.test.js
+++ src/k/kill.test.js
@@ -1,1 +1,12 @@
-expect(kill).to.be.a('function');
+it('basic', done => {
+    const childProcess = require('child_process');
+
+    const code = 'setInterval(() => {}, 1000 * 1000);';
+    const subProcess = childProcess.spawn('node', ['-e', code], {
+        detached: true
+    });
+
+    subProcess.on('exit', () => done());
+
+    kill(subProcess.pid);
+});
