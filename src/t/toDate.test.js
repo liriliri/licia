af5991ea525070b99931f10436a98f67376a6e91
surunzi <redhoodsu@tencent.com>
2020-05-24 16:47:17 +0800

    test: toDate

--- src/t/toDate.test.js
+++ src/t/toDate.test.js
@@ -5,4 +5,4 @@ expect(isDate(toDate())).to.be.true;
 expect(isDate(toDate(null))).to.be.true;
 expect(dateFormat(toDate('20180501'), 'yyyymmdd')).to.equal('20180501');
 expect(dateFormat(toDate('2018-05-01'), 'yyyymmdd')).to.equal('20180501');
-expect(dateFormat(toDate(1525107450849), 'yyyymm')).to.equal('201805');
+expect(dateFormat(toDate(1525197450849), 'yyyymm')).to.equal('201804');
