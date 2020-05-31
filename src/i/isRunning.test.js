e8bad7cc6c80d29e351efeb1b9e4888c854f4e74
redhoodsu <redhoodsu@tencent.com>
2020-05-31 22:26:15 +0800

    test(isRunning): small changes

--- src/i/isRunning.test.js
+++ src/i/isRunning.test.js
@@ -1,14 +1,10 @@
 const cp = require('child_process');
 
-const child = cp.spawn(
-    'node',
-    ['-e', 'setInterval(() => console.log("running"), 1000)'],
-    {
-        detached: true,
-        stdio: 'ignore'
-    }
-);
+const child = cp.spawn('node', [
+    '-e',
+    'setInterval(() => console.log("running"), 1000)'
+]);
 
 expect(isRunning(child.pid)).to.be.true;
-util.kill(child.pid);
-expect(isRunning(child.pid)).to.be.false;
+child.kill();
+expect(isRunning(1234567890)).to.be.false;
