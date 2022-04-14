79b3e8c2d336d7b39917a3138f7b10bc735ac94a
redhoodsu <redhoodsu@tencent.com>
2022-04-14 17:35:22 +0800

    feat(cgroup): support cpuset.cpus.effective

--- src/cgroup.js
+++ src/cgroup.js
@@ -17,11 +17,16 @@
  *             usage: number;
  *         };
  *     };
+ *     cpuset: {
+ *         cpus(): {
+ *             effective: number[];
+ *         };
+ *     };
  *     version(): number;
  * };
  */
 
-_('memoize each trim toNum');
+_('memoize each trim toNum contain concat range');
 
 const fs = require('fs');
 
@@ -42,6 +47,20 @@ const cpu = {
     }
 };
 
+const cpuset = {
+    cpus() {
+        let effective = [];
+
+        if (isV2()) {
+            effective = parseRange(read('cpuset.cpus.effective'));
+        }
+
+        return {
+            effective
+        };
+    }
+};
+
 const isV2 = memoize(function() {
     return fs.existsSync(resolve('cgroup.controllers'));
 });
@@ -50,6 +69,9 @@ function read(p) {
     return fs.readFileSync(resolve(p), 'utf8');
 }
 
+/* a 1
+ * b 2
+ */
 function parseKeyValue(data) {
     const ret = {};
 
@@ -64,12 +86,29 @@ function parseKeyValue(data) {
     return ret;
 }
 
+/* 1-2,4-5 */
+function parseRange(data) {
+    let ret = [];
+
+    each(data.split(','), r => {
+        if (!contain(r, '-')) {
+            ret.push(toNum(r));
+        } else {
+            r = r.split('-');
+            ret = concat(ret, range(toNum(r[0]), toNum(r[1]) + 1));
+        }
+    });
+
+    return ret;
+}
+
 function resolve(p) {
     return `/sys/fs/cgroup/${p}`;
 }
 
 exports = {
     cpu,
+    cpuset,
     version() {
         return isV2() ? 2 : 1;
     }
--- test/cgroup.js
+++ test/cgroup.js
@@ -1,2 +1,3 @@
 console.log('cgroup version', cgroup.version());
 console.log('cpu stat', cgroup.cpu.stat());
+console.log('cpuset cpus', cgroup.cpuset.cpus());
