1d61d16dec7ef65767014344e5ba99658d358044
redhoodsu <redhoodsu@tencent.com>
2023-01-10 11:13:05 +0800

    test(fileUrl): more test cases

--- test/fileUrl.js
+++ test/fileUrl.js
@@ -1,4 +1,6 @@
 tests([
     ['c:\\foo\\bar', 'file:///c:/foo/bar'],
-    ['/#foo/?bar', 'file:///%23foo/%3Fbar']
+    ['/#foo/?bar', 'file:///%23foo/%3Fbar'],
+    ['/redhood·su', 'file:///redhood%C2%B7su'],
+    ['/🐟', 'file:///%F0%9F%90%9F']
 ]);
