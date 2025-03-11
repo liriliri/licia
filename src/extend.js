2cd6b51bfdecd64e475251a67c3d86994446b8c1
redhoodsu <redhoodsu@tencent.com>
2025-03-11 10:51:23 +0800

    fix: extend ts definition

--- src/extend.js
+++ src/extend.js
@@ -16,6 +16,18 @@
  */
 
 /* typescript
+ * export declare function extend<T, T1>(destination: T, source1: T1): T & T1;
+ * export declare function extend<T, T1, T2>(
+ *     destination: T,
+ *     source1: T1,
+ *     source2: T2
+ * ): T & T1 & T2;
+ * export declare function extend<T, T1, T2, T3>(
+ *     destination: T,
+ *     source1: T1,
+ *     source2: T2,
+ *     source3: T3
+ * ): T & T1 & T2 & T3;
  * export declare function extend(destination: any, ...sources: any[]): any;
  */
 
